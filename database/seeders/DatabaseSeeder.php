e49fe81e47f7a5819b04839ee2dcf7ec55a7ab94
antonio <antonio.carlos@intecam.com.br>
2024-07-29 16:59:07 -0400

    laravel breeze, broadcasting, migrations and model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Group;
+use App\Models\Message;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,6 +20,30 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true
         ]);
+
+        User::factory()->create([
+            'name' => 'Test User2',
+            'email' => 'test2@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        User::factory(10)->create();
+
+        for($i = 0; $i < 5; $i++){
+            $group = Group::factory()->create([
+                'owner_id' => 1,
+            ]);
+            
+            $users = User::inRandomOrder()->limit(rand(2,5))->pluck('id');
+            $group->users()->attach(array_unique([1,...$users]));
+        } 
+        
+        Message::factory(1000)->create();
+        $messages = Message::whereNull('group_id')->orderBy('created_at')->get();
+        
+        $conversations = $messages->groupBy();
     }
 }
